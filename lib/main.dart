3be5d46ca8d14e8ae70c33c890f49e75a5c1e855
unknown <HasanAlphan>
2020-02-07 13:36:48 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_burcrehberi/burc_liste.dart';
+import 'package:flutter_burcrehberi/utils/testlib.dart';
+
+import 'burc_detay.dart';
+void main()=>runApp(MyApps());
+
+class MyApps extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: "Burc Rehberi",
+      debugShowCheckedModeBanner: false,
+      initialRoute: "/burcListesi",
+      routes: {
+        "/":(context) =>BurcListesi(),
+        "/burcListesi":(context) =>BurcListesi(),
+
+      },
+      onGenerateRoute: (RouteSettings settings){
+        List<String> pathElemanlari = settings.name.split("/");
+        if(pathElemanlari[1]=='burcDetay'){
+          return MaterialPageRoute(builder: (context)=>BurcDetay(int.parse(pathElemanlari[2])));
+        }
+        return null;
+      },
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+    );
+  }
+
+}
